9528cea1be322fa1f087b11f268981cbc36a8bec
tran_minh <tranminh1831996@gmail.com>
2024-11-04 11:20:57 +0900

    saved work

---
+++ prac.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+/*
+    cần phải suy nghĩ theo con trỏ
+    con trỏ kiểu gì
+    trò đi đâu
+    máy tính lưu trữ bằng ô nhớ
+    bản chất của việc này là kiểm soát sự tham chiếu đến các ô nhớ
+
+*/
+struct Node {
+    int data;
+    Node* next;
+}
+
+void insert_first(){
+
+}
+
+void insert_after(){
+    
+}
+
+void insert_end(){
+
+}
+
+int main(){
+
+    return 0;
+}
