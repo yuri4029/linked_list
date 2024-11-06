33149119e1a23c3a1fe90d03d33c883d6b5b54b7
yuri4029 <tranminh1831996@gmail.com>
2024-11-06 16:51:18 +0900

    Update linked_list.cpp

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,46 +1,145 @@
-#include <bits/stdc++.h>
-#define MAXN 100005
-#define INF 1000000000000000005
-#define mod 1000000007
-#define rep(i,n) for (int i=0;i<n;i++)
-#define rep1(i,n) for (int i=1;i<=n;i++)
-#define ll long long
-#define debug(x) cout << #x << ": "; _print(x); cout<<endl;
-
-
+#include <iostream>
 
 using namespace std;
 
-int dx[] ={ 0, 1, 0, -1}; // left, up, right, down
-int dy[] ={-1, 0, 1,  0};
-
-int dxx[] ={-1,-1, 1,  1}; // up left, down left, down right,  up right
-int dyy[] ={-1, 1, 1, -1};
-
-template<class T>  void _print(vector<int> v1){ for (int i : v1) cout<< i<< " ";}
-void _print(long long a) {cout<< a;}
-void _print(char a) {cout<< a;}
-void _print(string  a) {cout<< a;}
-void _print(bool a) {cout<< a;}
-
-struct node{
+struct Node
+{
     int data;
-    struct node *link;
+    Node *next;
+
+    // Node(int data) : data(data), next(nullptr) {}
 };
 
+/*
+Node* createNode (int data){
+    return new  Node(data);
+}
+*/
 
-int main (){
-	
-	freopen("inp.txt","r",stdin);
-	freopen("out.txt","w",stdout);
-	// g++ -o c c.cpp
-    struct node *head = malloc(sizeof(struct node));
-    head->data = 45;
-    head->link = NULL;
+void insert_first(Node **head_ref, int new_data)
+{
+    Node *newNode;
+    newNode = new Node(); // ham tao
+    newNode->data = new_data;
 
-    struct node *end = malloc(sizeof(struct node));
-    end->data =-1;
-    head->link =NULL;
+    newNode->next = *head_ref;
+    *head_ref = newNode;
+}
 
-	return 0;
-} 
+void print_list(Node *head_ref)
+{
+    while (head_ref != nullptr)
+    {
+        std::cout << head_ref->data << " ";
+        head_ref = head_ref->next;
+    }
+}
+
+void print_list_1(Node **head_ref)
+{
+    Node* temp = *head_ref; 
+    
+    while (temp != nullptr)
+    {
+        std::cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+int countNodes(Node* head){
+    int count =0;
+    Node* temp = head;
+    while(temp!=nullptr){
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
+
+void insert_last(Node *&head, int data)
+{
+    //Node* newNode = createNode(data);
+    Node *newNode = new Node();
+    if (head == nullptr)
+    {
+        head = newNode;
+        return;
+    }
+    Node *temp = head;
+    while (temp->next != nullptr)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void insert_last_1(Node **head_ref, int new_data)
+{
+    Node *new_node = new Node();
+    new_node->data = new_data;
+    new_node->next = nullptr;
+    if (*head_ref == nullptr) {
+        *head_ref = new_node;
+        return;
+    }
+    Node *last = *head_ref; // Used to traverse to the end of the list
+    while (last->next != nullptr) {
+        last = last->next;
+    }
+    last->next = new_node;
+}
+
+void insert_at(Node** head,int pos, int new_data){
+    Node* newNode = new Node();
+    newNode->data = new_data;
+    newNode->next = nullptr;
+
+    if (pos==0){
+        newNode->next = *head;
+        *head= newNode;
+        return;
+    }
+
+    Node* temp = *head;
+
+    
+
+    for (int i = 0; temp != nullptr && i < pos - 1; i++) {
+        temp = temp->next;
+         //cout<<temp->data<<endl;
+         //cout<<i<<endl;
+    }
+    if (temp == nullptr) {
+        cout << "Position out of range" << endl;
+        return;
+    } 
+    
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+
+int main()
+{
+
+    Node *head = nullptr;
+    
+    
+    insert_at(&head,0,4);
+    insert_first(&head, 3);
+    insert_first(&head, 2);
+    insert_first(&head, 1);
+
+    print_list(head);cout<<endl;
+
+    insert_at(&head,3,13);
+    insert_at(&head,1,11);
+
+    //print_list_1(&head);cout<<endl;
+    
+
+    print_list(head);cout<<endl;
+    cout<<"amout:"<<countNodes(head);
+
+    return 0;
+}
