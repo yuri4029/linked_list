42ceb3040a01d3e37e7dde39b32571851137bc3c
yuri4029 <tranminh1831996@gmail.com>
2024-11-07 22:59:44 +0900

    Changes to be committed: 	modified:   prac.cpp 	modified:   prac_out.cpp

--- prac_out.cpp
+++ prac_out.cpp
@@ -1,33 +1,7 @@
 #include <iostream>
 using namespace std;
 /*
-    cần phải suy nghĩ theo con trỏ
-    con trỏ kiểu gì
-    trò đi đâu
-    con trỏ lưu trữ thông tin của 2 loại ô nhớ
-        - ô nhớ của chính nó
-        - ô nhớ mà nó đang trỏ vào
-
-    máy tính lưu trữ bằng ô nhớ
-    bản chất của việc này là kiểm soát sự tham chiếu đến các ô nhớ
-
-    pointer explain
-
-    int var = 9;
-    int* p;
-    cout<<"var mem cell: "<<&var <<endl;
-
-    cout<<endl<<"before assign"<<endl;
-    cout<<"mem cell that pointer is pointing :"<<p<<endl;
-    cout<<"value of pointer :"<<*p<<endl;
-    cout<<"mem cell of pointer itself :"<<&p<<endl;
-
-    p=&var;
-
-    cout<<endl<<"after assign"<<endl;
-    cout<<"mem cell that pointer is pointing :"<<p<<endl;
-    cout<<"value of pointer :"<<*p<<endl;
-    cout<<"mem cell of pointer itself :"<<&p<<endl;
+  
 
 */
 struct Node
@@ -35,30 +9,6 @@ struct Node
     int data;
     Node *next;
 
-    /*
-        | data | pointer |
-        linked list sẽ cần 1 con trỏ là header,
-        header sẽ định nghĩa phần tử đầu tiên của list
-
-        khi thêm 1 phần tử vào đầu linked list thì
-        - phần tử mới trỏ đến head
-        - copy head sang phần tử mới
-
-        head -> null
-
-        add node 1 :
-
-        node_1->head->null
-        head = node_1
-
-        head->null
-        node_1->null
-
-        add node 2 :
-        node_2->head( node_1 info)
-        head = node_2
-
-     */
 };
 
 void insert_first(Node** head_ref , int new_data)
